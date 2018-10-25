cb94527c815166bd91e6bf47bd6586e0c784b187
helenkramar <elena.kramar@hotmail.com>
2018-10-25 22:28:49 +0300

    Test into master (itegration project + consumer test) (#3)
    Test into master (itegration project + consumer test)

---
+++ test/integration/Application/TestStartup.cs
@@ -0,0 +1,13 @@
+﻿using consumer;
+
+using Microsoft.Extensions.Configuration;
+
+namespace integration.Application
+{
+    public class TestStartup : Startup
+    {
+        public TestStartup(IConfiguration configuration) : base(configuration)
+        {
+        }
+    }
+}
