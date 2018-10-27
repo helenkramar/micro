21d8aadffbb39c9abf79d2df636bbd8236df0e5d
helenkramar <elena.kramar@hotmail.com>
2018-10-27 11:29:50 +0300

    move test project to microservices

---
+++ microservices/integration/Models/ProviderMock.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace integration.Models
+{
+    public class ProviderMock
+    {
+        public string Name;
+        public Uri Uri;
+        public int MockPort;
+    }
+}
